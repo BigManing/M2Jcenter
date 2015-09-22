68455339065ebb24fd1545463ecd5d4165d7e7af
admin <lingshui2008@qq.com>
2015-09-22 15:27:57 +0800

    新建项目，以及自己依赖库，Library里面定义一个方法

---
+++ jcenterlibrary/src/main/java/jscenter/test/com/jcenterlibrary/ToastUtil.java
@@ -0,0 +1,14 @@
+package jscenter.test.com.jcenterlibrary;
+
+import android.content.Context;
+import android.widget.Toast;
+
+/**
+ * <P>  <P>  <P>  <P>
+ * Created by JiangShang on 2015/9/22.
+ */
+public class ToastUtil {
+    public static void toast(String str, Context context) {
+        Toast.makeText(context,str,Toast.LENGTH_SHORT).show();
+    }
+}
