39b4beb9472d3cc2b44b40e3f7148a61a041388c
admin <lingshui2008@qq.com>
2015-09-22 15:32:08 +0800

    新建项目，以及自己依赖库，Library里面定义一个方法

---
+++ app/src/androidTest/java/jscenter/test/com/m2jcenter/ApplicationTest.java
@@ -0,0 +1,13 @@
+package jscenter.test.com.m2jcenter;
+
+import android.app.Application;
+import android.test.ApplicationTestCase;
+
+/**
+ * <a href="http://d.android.com/tools/testing/testing_android.html">Testing Fundamentals</a>
+ */
+public class ApplicationTest extends ApplicationTestCase<Application> {
+    public ApplicationTest() {
+        super(Application.class);
+    }
+}
